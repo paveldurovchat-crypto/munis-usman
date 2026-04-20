b40e65cfb798584ef3e43a48606c44e3f163f8d7
paveldurovchat-crypto <paveldurovchat-crypto@users.noreply.github.com>
2026-04-20 19:31:49 +0000

    deploy: 13f19e2a5364d4859582ba352381cfca144c0262

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import { $, a0 } from "./assets/worker-entry-BkQn58Dh.js";
-import "node:events";
-import "node:async_hooks";
-import "node:stream/web";
-import "node:stream";
-export {
-  $ as createServerEntry,
-  a0 as default
-};
